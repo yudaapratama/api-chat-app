dcbd0955cd55c23ba4c48600feec60f024a0474b
yudaa_pratama <yudhaprathamaa@gmail.com>
2023-01-03 16:19:10 +0700

    make model & migration for room

---
+++ database/migrations/2023_01_03_064711_create_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sender')->unsigned();
+            $table->integer('receiver')->unsigned();
+            $table->unique(['sender', 'receiver']);
+            $table->foreign('sender')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('receiver')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
