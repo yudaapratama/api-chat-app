3e4048d678a112e37723fddc96851af79c975f29
yudaa_pratama <yudhaprathamaa@gmail.com>
2023-01-03 22:14:19 +0700

    modify room model

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'sender',
+        'receiver'
+    ];
+
+    protected $cast = [
+        'receiver' => 'integer',
+        'sender' => 'integer'
+    ];
+
+    public function scopeChatRooms($query)
+    {
+        return $query->where('sender', auth()->user()->id)->orWhere('receiver', auth()->user()->id);
+    }
+
+    public function chats()
+    {
+        return $this->hasMany(Chat::class);
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender', 'id');
+    }
+
+    public function receiver() 
+    {
+        return $this->belongsTo(User::class, 'receiver', 'id');
+    }
+
+    
+
 }
