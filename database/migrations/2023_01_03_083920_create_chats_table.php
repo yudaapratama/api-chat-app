6e64e72df00ba5c2b8a518f3ae206ea24d7c65a8
yudaa_pratama <yudhaprathamaa@gmail.com>
2023-01-03 16:19:58 +0700

    make model & migration for chat

---
+++ database/migrations/2023_01_03_083920_create_chats_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('type');
+            $table->integer('room_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->integer('message_id')->unsigned();
+            $table->dateTime('read_at');
+            $table->foreign('room_id')->references('id')->on('rooms')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('message_id')->references('id')->on('messages')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+};
