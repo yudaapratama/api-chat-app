7e2e26d2c4a5ddbfa925184751e310dac24e0e6f
yudaa_pratama <yudhaprathamaa@gmail.com>
2023-01-03 22:13:13 +0700

    create login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Traits\ResponseApi;
+use Exception;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -12,11 +15,46 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $data = Validator::make($request->all(), [
-            'username' => 'required',
-            'password' => 'required'
-        ]);
+        try {
+            
+            $data = Validator::make($request->all(), [
+                'username' => 'required|email',
+                'password' => 'required'
+            ]);
 
-        dd($data);
+            if($data->fails()) {
+                return $this->error($data->errors(), 400);
+            }
+
+            $user = User::where('username', $request->username)->first();
+
+            if(!$user) {
+                return $this->error('user not found', 404);
+            }
+
+            if(!Auth::attempt($request->only(['username', 'password']))) {
+                return $this->error('email or password doesn\'t match');
+            }
+
+            $token = $user->createToken('th!sT0k3n')->plainTextToken;
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Login successfully.',
+                'data' => [
+                    'name' => $user->name,
+                    'access_token' => $token,
+                    'token_type' => 'Bearer'
+                ]
+            ]);
+
+
+        } catch (Exception $e) {
+
+            return $this->error($e->getMessage(), 500);
+
+        }
+
+        
     }
 }
