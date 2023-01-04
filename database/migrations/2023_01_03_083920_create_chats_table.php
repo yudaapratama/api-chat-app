28d18647d1c7958ea69b1d9d479e85d231335574
yudaa_pratama <yudhaprathamaa@gmail.com>
2023-01-04 17:52:18 +0700

    add nullable for column read_at

--- database/migrations/2023_01_03_083920_create_chats_table.php
+++ database/migrations/2023_01_03_083920_create_chats_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->integer('room_id')->unsigned();
             $table->integer('user_id')->unsigned();
             $table->integer('message_id')->unsigned();
-            $table->dateTime('read_at');
+            $table->dateTime('read_at')->nullable();
             $table->foreign('room_id')->references('id')->on('rooms')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('message_id')->references('id')->on('messages')->onUpdate('cascade')->onDelete('cascade');
