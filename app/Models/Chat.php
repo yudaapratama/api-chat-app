fa6ae694349f31342eb3c978284ac826dd420042
yudaa_pratama <yudhaprathamaa@gmail.com>
2023-01-04 17:51:18 +0700

    add methods in chat model

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Chat extends Model
 {
     use HasFactory;
+
+    protected $with = [
+        'message'
+    ];
+
+    protected $fillable = [
+        'type',
+        'room_id',
+        'user_id',
+        'message_id',
+        'read_at'
+    ];
+
+    public function scopeUserChat($query)
+    {
+        return $query->where('user_id', auth()->user()->id);
+    }
+
+    public function message()
+    {
+        return $this->belongsTo(Message::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
