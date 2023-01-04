93403e722520fd228018605195a6edb8312bbbc4
yudaa_pratama <yudhaprathamaa@gmail.com>
2023-01-04 17:53:43 +0700

    add brodcast channel

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Room;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('chat-{room}', function ($user, Room $room) {
+    if($user->id == $room->sender || $user->id == $room->receiver) {
+        return true;
+    }
+    return false;
 });
